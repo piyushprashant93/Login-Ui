add6f5bf1ed73982a1eca9c82341184cb778e5fd
piyushprashant93 <piyushprashant93@gmail.com>
2021-11-17 13:00:56 +0530

    Creation of login ui

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,75 @@
 import Head from 'next/head'
-
+import { useEffect,useState } from 'react'
+import { FaFacebookF, FaLinkedinIn, FaGoogle, FaRegEnvelope } from "react-icons/fa"
+import {MdLockOutline} from "react-icons/md"
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+  },[])
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-100">
       <Head>
-        <title>Create Next App</title>
+        <title>Login Ui -Next.js and Tailwind css</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+        <div className={`bg-white rounded-2xl shadow-2xl flex flex-row  w-2/3 max-w-4xl ${isLoading? 'animate-pulse':'animate-none'}`}>
+          <div className="w-3/5 p-5">
+            <div className="text-left font-bold"> <span className="text-blue-500">Company</span>Name</div>
+            <div className="py-10">
+              <h2 className="text-3xl font-bold text-blue-500">Sign in to Account</h2>
+              <div className="border-2 w-20 inline-block border-blue-500 mb-2 rounded-2xl"></div>
+              <div className="flex justify-center my-2 ">
+                <a href="#" className="border-2 border-gray-200 rounded-full p-3 mx-1" >
+                  <FaFacebookF className="text-sm" />
+                </a>
+                <a href="#" className="border-2 border-gray-200 rounded-full p-3 mx-1" >
+                  <FaLinkedinIn className="text-sm" />
+                </a>
+                <a href="#" className="border-2 border-gray-200 rounded-full p-3 mx-1" >
+                  <FaGoogle className="text-sm" />
+                </a>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+              </div>
+              <p className="text-gray-400 my-3">or use you email account</p>
+              <div className="flex flex-col items-center">
+                <div className="bg-gray-100 w-64 p-2  flex items-center mb-3"> 
+                <FaRegEnvelope className="text-sm m-2 text-gray-400" />
+                <input  type="email"  name="email" placeholder="Enter your email" className ="bg-gray-100 outline-none text-sm flex-1" />
+                </div>
+                <div className="bg-gray-100 w-64 p-2  flex items-center mb-3"> 
+                <MdLockOutline className="text-sm m-2 text-gray-400" />
+                <input  type="password"  name="password" placeholder="Enter your password" className ="bg-gray-100 outline-none text-sm flex-1" />
+                </div>
+                <div className="flex justify-between w-64 mb-5">
+                  <label className="flex items-center text-xs  text-gray-400">
+                    <input type="checkbox" className="mr-1" name="remember" />
+                    remember me
+                  </label>
+                  <a href="#" className="text-xs"> Forgot password?</a>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+                </div>
+                <a href="#" className="border-2 border-blue-500 text-blue-500 rounded-full px-12 py-2 inline-block font-semibold hover:bg-blue-500 hover:text-white">Sign In</a>
+              </div>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            </div>
+          </div>{/**
+             * Sign in section
+             */}
+          <div className="w-2/5  bg-blue-500 text-white rounded-tr-2xl rounded-br-2xl py-36 px-12">
+            <h2 className="text-3xl font-bold mb-2">Hello, Friends</h2>
+            <div className="border-2 w-20 inline-block border-white mb-2 rounded-2xl"></div>
+            <p className="mb-10">Fill up personal information and start journey with us.</p>
+            <a href="#" className="border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-blue-500">Sign up</a>
+          </div>{/**
+              * Sign up section
+              * **/}
         </div>
-      </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      </main>
     </div>
   )
 }
